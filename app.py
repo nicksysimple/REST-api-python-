c9c51b1d386c3f363b0dd43bbd40773e4fdeb426
Nicksysimple <nicksonkipkorir45@gmail.com>
2021-06-29 19:49:40 +0300

    created endpoints for products

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 from flask import Flask
 
 app = Flask(__name__)
+#create a store
+@app.route('/store',methods = ['POST'])
+def create_store():
+    pass
 
-@app.route('/')
-def home():
-    return "hello world"
+#fetch a store
+app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+
+#get stores
+app.route('/store')
+def get_store():
+    pass
+
+#create an item
+@app.route('/store/<string:name>/item',method = ['POST'])
+def create_item_in_store():
+    pass
+
+
+#get items in store
+app.route('/store/<string:name>/item')
+def get_items_in_store():
+    pass
 
 
 app.run(port=5000)
